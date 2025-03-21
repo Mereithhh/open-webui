7fde341111042430c7e20f86d4f57b8a43a72654
zhangzichen <zichen.zhang@aminer.cn>
2025-03-21 18:36:59 +0800

    feat: 分享页面支持手动预览artifacts，默认打开

--- src/routes/s/[id]/+page.svelte
+++ src/routes/s/[id]/+page.svelte
@@ -17,6 +17,8 @@
 	import { getModels } from '$lib/apis';
 	import { toast } from 'svelte-sonner';
 	import localizedFormat from 'dayjs/plugin/localizedFormat';
+	import { showArtifacts } from '$lib/stores';
+	import Artifacts from '$lib/components/chat/Artifacts.svelte';
 
 	const i18n = getContext('i18n');
 	dayjs.extend(localizedFormat);
@@ -136,7 +138,7 @@
 		class="h-screen max-h-[100dvh] w-full flex flex-col text-gray-700 dark:text-gray-100 bg-white dark:bg-gray-900"
 	>
 		<div class="flex flex-col flex-auto justify-center relative">
-			<div class=" flex flex-col w-full flex-auto overflow-auto h-0" id="messages-container">
+			<div class=" flex flex-col w-full flex-auto h-0" id="messages-container">
 				<div class="pt-5 px-2 w-full max-w-5xl mx-auto">
 					<div class="px-3">
 						<div class=" text-2xl font-semibold line-clamp-1">
@@ -151,24 +153,31 @@
 					</div>
 				</div>
 
-				<div class=" h-full w-full flex flex-col py-2">
-					<div class="">
-						<Messages
-							className="h-full flex pt-4 pb-8"
-							{user}
-							chatId={$chatId}
-							readOnly={true}
-							{selectedModels}
-							{processing}
-							bind:history
-							bind:messages
-							bind:autoScroll
-							bottomPadding={files.length > 0}
-							sendPrompt={() => {}}
-							continueResponse={() => {}}
-							regenerateResponse={() => {}}
-						/>
+				<div class="flex flex-1 w-full h-full mx-auto">
+					<div class=" h-full w-full flex flex-1 flex-col overflow-auto py-2">
+						<div class="">
+							<Messages
+								className="h-full flex pt-4 pb-8"
+								{user}
+								chatId={$chatId}
+								readOnly={true}
+								{selectedModels}
+								{processing}
+								bind:history
+								bind:messages
+								bind:autoScroll
+								bottomPadding={files.length > 0}
+								sendPrompt={() => {}}
+								continueResponse={() => {}}
+								regenerateResponse={() => {}}
+							/>
+						</div>
 					</div>
+					{#if $showArtifacts}
+						<div class="artifactsContainer flex-1 relative h-full">
+							<Artifacts {history} />
+						</div>
+					{/if}
 				</div>
 			</div>
 
@@ -181,6 +190,12 @@
 						on:click={cloneSharedChat}
 					>
 						{$i18n.t('Clone Chat')}
+					</button>
+					<button
+						class="px-4 py-2 text-sm font-medium bg-black hover:bg-gray-900 text-white dark:bg-white dark:text-black dark:hover:bg-gray-100 transition rounded-full"
+						on:click={() => showArtifacts.set(!$showArtifacts)}
+					>
+						预览Artifacts
 					</button>
 				</div>
 			</div>
